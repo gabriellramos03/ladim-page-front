88f1053857b02ef78fe9777735a3e07f10642835
Gabriel <gabrielbatista@seduc.ro.gov.br>
2025-02-06 14:26:35 -0400

    Ajuste rota principal temporario

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,50 @@
-import Image from "next/image";
+'use client';
+
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation'; 
 
 export default function Home() {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true); 
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/login');
+      setIsLoading(false); 
+    }, 1500); 
+  }, [router]);
+
   return (
-    <div>
-      <h1>teste</h1>
+    <div style={styles.container}>
+      {isLoading ? (
+        <div style={styles.spinner}></div> 
+      ) : null}
     </div>
   );
 }
+
+const styles = {
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    textAlign: 'center',
+  },
+  spinner: {
+    border: '8px solid #f3f3f3', 
+    borderTop: '8px solid #3498db',
+    borderRadius: '50%',
+    width: '50px',
+    height: '50px',
+    animation: 'spin 1s linear infinite',
+  },
+  '@keyframes spin': {
+    '0%': {
+      transform: 'rotate(0deg)',
+    },
+    '100%': {
+      transform: 'rotate(360deg)',
+    },
+  },
+};
